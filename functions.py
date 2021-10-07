c33d0cfa315a5db25aaf15efae1c0244d4c02c0b
danielgarciah <danielgarciah@live.com.mx>
2021-10-06 23:02:40 -0500

    get_historical

---
+++ functions.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import MetaTrader5 as Mt5
+
+
+class log_meta():
+
+    def __init__(self, path, login, password, server, start_date, end_date):
+        self.path = path
+        self.login = login
+        self.password = password
+        self.server = server
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def f_login(self):
+        connection = Mt5.initialize(path=self.path,
+                                    login=self.login,
+                                    password=self.password,
+                                    server=self.server)
+        if connection:
+            print("Si funciona")
+        else:
+            print(Mt5.last_error())
+            Mt5.shutdown()
+        return Mt5
+
+    def account_info(self):
+        return self.f_login().account_info()
+
+    def get_historical(self):
+        tuplas = self.f_login().history_deals_get(self.start_date, self.end_date)
+        df = pd.DataFrame(tuplas,
+                          columns=['ticket', 'order', 'time', 'time_msc', 'type', 'entry', 'magic', 'position_id',
+                                   'reason', 'volume', 'price', 'commission', 'swap', 'profit', 'fee', 'symbol',
+                                   'comment', 'external_id'])
+        return df
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-import numpy as np
+from functions import log_meta
+from datetime import datetime
+
+
+meta_path = 'C:\Program Files\MetaTrader 5 Terminal\\terminal64.exe'
+login_count = 5400339
+pasword_count = '2qeDQrhu'
+server_name = 'FxPro-MT5'
+start_date = datetime(2021, 8, 1)
+end_date = datetime.today()
+
+log = log_meta(meta_path, login_count, pasword_count, server_name, start_date, end_date)
 
 if __name__ == "__main__":
-    print("Hello_World")
-    
+    # print("Hello_World")
+    print(log.f_login())
+    print(log.account_info())
+    print(log.get_historical())
+
+
