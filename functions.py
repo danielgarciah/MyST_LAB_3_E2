ff347e5906b6d9f51edeb23df42ed27fc56f9de5
ChelsiSedano <chelsi.sg@hotmail.com>
2021-10-08 21:11:54 -0500

    DealsAndOrders

--- functions.py
+++ functions.py
@@ -28,24 +28,38 @@ class log_meta():
     def account_info(self):
         return self.f_login().account_info()
 
-    def get_historical(self, save_name: Optional[str] = None):
+    def get_historical_deals(self, save_name: Optional[str] = None):
         tuplas = self.f_login().history_deals_get(self.start_date, self.end_date)
         df = pd.DataFrame(tuplas, columns=tuplas[0]._asdict().keys())
         df['time'] = pd.to_datetime(df['time'], unit='s')
         df['time_msc'] = pd.to_datetime(df['time'], unit='ms')
         if save_name:
-            reportpath = path.abspath('Reportes_MT5/') + "/"
+            reportpath = path.abspath('ReportesDeals_MT5/') + "/"
             df.to_excel(reportpath + save_name + ".xlsx")
         return df
+    
+    def get_historical_orders(self, save_name2: Optional[str] = None):
+        tuplas2 = self.f_login().history_orders_get(self.start_date, self.end_date)
+        df2 = pd.DataFrame(tuplas2, columns=tuplas2[0]._asdict().keys())
+        #df2['time'] = pd.to_datetime(df2['time'], unit='s')
+        #df2['time_msc'] = pd.to_datetime(df2['time'], unit='ms')
+        if save_name2:
+            reportpath2 = path.abspath('ReportesOrders_MT5/') + "/"
+            df2.to_excel(reportpath2 + save_name2 + ".xlsx")
+        return df2
 
 class load_excel():
 
     def __init__(self, file_name):
         self.file_name = file_name
 
-    def get_historical(self):
-        reportpath = path.abspath('Reportes_MT5/') + "/"
+    def get_historical_deals(self):
+        reportpath = path.abspath('ReportesDeals_MT5/') + "/"
         return pd.read_excel(reportpath + self.file_name + ".xlsx")
+    
+    def get_historical_orders(self):
+        reportpath2 = path.abspath('ReportesOrders_MT5/') + "/"
+        return pd.read_excel(reportpath2 + self.file_name + ".xlsx")
 
 
 class est_desc():
